733b70767dfc7bf296afa20cd5e8d7ad570f082c
Carlos Goncalves <carlos_henrique97@outlook.com.br>
2026-03-13 22:29:03 -0300

    fix: repair on old strategy

--- VideoProcessingMs/Infra.Data.SqlServer/EnsureDatabaseExists.cs
+++ VideoProcessingMs/Infra.Data.SqlServer/EnsureDatabaseExists.cs
@@ -8,9 +8,6 @@ namespace Infra.Data.SqlServer
         {
             Console.WriteLine("Verificando existência da base de dados...");
 
-            if (string.IsNullOrWhiteSpace(dbName))
-                throw new ArgumentException("Nome do banco de dados não informado.", nameof(dbName));
-
             bool dbExists = false;
 
             var builder = new SqlConnectionStringBuilder(connectionString)
@@ -25,38 +22,36 @@ namespace Infra.Data.SqlServer
 
                 using var checkCmd = connection.CreateCommand();
                 checkCmd.CommandText = @"
-                    SELECT CASE
-                        WHEN DB_ID(@DbName) IS NULL THEN 0
-                        ELSE 1
+                    SELECT CASE 
+                        WHEN DB_ID('VideoUploadDb') IS NULL THEN 0 
+                        ELSE 1 
                     END";
-                checkCmd.Parameters.AddWithValue("@DbName", dbName);
 
                 dbExists = (int)checkCmd.ExecuteScalar() == 1;
 
                 if (!dbExists)
                 {
                     using var createCmd = connection.CreateCommand();
-                    createCmd.CommandText = $"CREATE DATABASE [{dbName}];";
+                    createCmd.CommandText = "CREATE DATABASE VideoUploadDb;";
                     createCmd.ExecuteNonQuery();
-                }
 
-                connection.ChangeDatabase(dbName);
+                    string assemblyDir = Path.GetDirectoryName(typeof(DatabaseInitializer).Assembly.Location)!;
+                    string scriptPath = Path.Combine(assemblyDir, "schema.sql");
+                    string script = File.ReadAllText(scriptPath);
+                    var command = connection.CreateCommand();
 
-                string assemblyDir = Path.GetDirectoryName(typeof(DatabaseInitializer).Assembly.Location)!;
-                string scriptPath = Path.Combine(assemblyDir, "schema.sql");
-                string script = File.ReadAllText(scriptPath);
+                    var batches = script.Split(
+                        new[] { "\r\nGO\r\n", "\nGO\n", "\rGO\r" },
+                        StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
 
-                var batches = script.Split(
-                    new[] { "\r\nGO\r\n", "\nGO\n", "\rGO\r" },
-                    StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
-
-                foreach (var batch in batches)
-                {
-                    if (!string.IsNullOrWhiteSpace(batch))
+                    foreach (var batch in batches)
                     {
-                        var batchCommand = connection.CreateCommand();
-                        batchCommand.CommandText = batch;
-                        batchCommand.ExecuteNonQuery();
+                        if (!string.IsNullOrWhiteSpace(batch))
+                        {
+                            var batchCommand = connection.CreateCommand();
+                            batchCommand.CommandText = batch;
+                            batchCommand.ExecuteNonQuery();
+                        }
                     }
                 }
             }
